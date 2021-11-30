0b11c5ff8285ecb1137ac991417be7a9a09c7e87
Francesco Foschini <francesco.foschini6@studio.unibo.it>
2021-11-30 15:41:40 +0100

    Add exception for DbManager

---
+++ RethinkDbApp/RethinkDbLib/src/Exception/DeleteTableSystemException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RethinkDbApp.Exception
+{
+    /// <summary>
+    /// Se si tenta di eliminare una tabella di sistema
+    /// </summary>
+    [Serializable]
+    class DeleteTableSystemException : System.Exception
+    {
+        private readonly static string message = "Impossibile eliminare la tabella di sistema ";
+        public DeleteTableSystemException(string table) : base(message + table)
+        {
+
+        }
+    }
+}
---
+++ RethinkDbApp/RethinkDbLib/src/Exception/TableNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RethinkDbApp.Exception
+{
+    /// <summary>
+    /// Se la Tabella non esiste o non è stata trovata sul db Rethink
+    /// </summary>
+    [Serializable]
+    class TableNotFoundException : System.Exception
+    {
+        private readonly static string message = "Tabella ";
+        private readonly static string message2 = " non trovata sul db";
+        public TableNotFoundException(string table) : base(message + table + message2)
+        {
+
+        }
+    }
+}
