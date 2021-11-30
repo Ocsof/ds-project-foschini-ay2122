2a847324c11fa37d2c2365eba30ce906251396d4
Francesco Foschini <francesco.foschini6@studio.unibo.it>
2021-11-30 15:40:24 +0100

    DbManagerImplemented

---
+++ RethinkDbApp/RethinkDbLib/src/DbManager/DbManager.cs
@@ -0,0 +1,120 @@
+﻿using Rethink.Connection;
+using RethinkDb.Driver;
+using RethinkDbApp.Exception;
+using System.Linq;
+
+namespace Rethink.Model
+{
+    class DbManager : IDbManager
+    {
+        private readonly IConnectionNodes connection;
+        private readonly static RethinkDB R = RethinkDB.R;
+        private readonly string dbName;
+        private string[] wellKnowns;
+
+        public DbManager(IConnectionNodes connection, string[] wellKnown)  //IConnectionPooling connectionFactory  // ---> per connessione con un cluster + nodi
+        {
+            this.connection = connection;
+            this.dbName = connection.GetNodi().ElementAt(0).Database;
+            this.wellKnowns = wellKnown;
+        }
+
+        public string GetTablesList()
+        {
+            var conn = this.connection.GetConnection();
+            var tableList = R.Db(dbName).TableList().Run(conn);
+
+            return tableList.ToString();
+        }
+
+        public void CreateTable(string tableName)
+        {
+            var conn = this.connection.GetConnection();
+
+            var exists = R.Db(dbName).TableList().Contains(t => t == tableName).Run(conn);
+            if (!exists)
+            {
+                R.Db(this.dbName).TableCreate(tableName).Run(conn);
+                R.Db(this.dbName).Table(tableName).Wait_().Run(conn);
+            }
+        }
+
+        public void DeleteTable(string tableName)
+        {
+            if (wellKnowns.Contains(tableName))
+            {
+                throw new DeleteTableSystemException(tableName);
+            }
+            var conn = this.connection.GetConnection();
+            var exists = R.Db(this.dbName).TableList().Contains(t => t == tableName).Run(conn);
+            if (exists)
+            {
+                R.Db(this.dbName).TableDrop(tableName).Run(conn);
+            }
+        }
+
+        public string GetIndexList(string tableName) 
+        {
+            var conn = this.connection.GetConnection();
+            try
+            {
+                var indexList = R.Db(this.dbName).Table(tableName).IndexList().Run(conn);
+                return indexList.ToString();
+            }
+            catch (ReqlOpFailedError)
+            {
+                throw new TableNotFoundException(tableName);
+            }
+        }
+
+        public void CreateIndex(string tableName, string indexName)
+        {
+            var conn = this.connection.GetConnection();
+            try
+            {
+                var exists = R.Db(this.dbName).Table(tableName).IndexList().Contains(t => t == indexName).Run(conn);
+                if (!exists)
+                {
+                    R.Db(this.dbName).Table(tableName).IndexCreate(indexName).Run(conn);
+                    R.Db(this.dbName).Table(tableName).IndexWait(indexName).Run(conn);
+                }
+            }
+            catch (ReqlOpFailedError)
+            {
+                throw new TableNotFoundException(tableName);
+            }
+        }
+
+        public void DeleteIndex(string tableName, string indexName)
+        {
+            var conn = this.connection.GetConnection();
+            try
+            {
+                var exists = R.Db(this.dbName).Table(tableName).IndexList().Contains(t => t == indexName).Run(conn);
+                if (exists)
+                {
+                    R.Db(this.dbName).Table(tableName).IndexDrop(indexName).Run(conn);
+                }
+            }
+            catch (ReqlOpFailedError)
+            {
+                throw new TableNotFoundException(tableName);
+            }
+        }
+
+        public void ReconfigureTable(string tableName, int shards, int replicas)
+        {
+            var conn = this.connection.GetConnection();
+            try
+            {
+                R.Db(this.dbName).Table(tableName).Reconfigure().OptArg("shards", shards).OptArg("replicas", replicas).Run(conn);
+                R.Db(this.dbName).Table(tableName).Wait_().Run(conn);
+            }
+            catch (ReqlOpFailedError)
+            {
+                throw new TableNotFoundException(tableName);
+            }
+        }
+ 
+    }
+}
---
+++ RethinkDbApp/RethinkDbLib/src/DbManager/IDbManager.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace Rethink.Model
+{
+    /// <summary>
+    /// Gestione db ---> tabelle, indici, riconfigurazione shards e replication
+    /// </summary>
+    public interface IDbManager
+    {
+        
+        /// <summary>
+        /// Restituisce le tabelle presenti sul db
+        /// </summary>
+        /// <returns>Tabelle presenti sul db</returns>
+        public string GetTablesList();
+
+        /// <summary>
+        /// Crea la tabella sul db precedentemente specificato.
+        /// </summary>
+        /// <param name="tableName">Nome tabella da creare sul db</param>
+        public void CreateTable(string tableName);
+
+        /// <summary>
+        /// Elimina la tabella sul db specificato inizialmente
+        /// </summary>
+        /// <param name="tableName">Nome tabella da eliminare</param>
+        public void DeleteTable(string tableName);
+
+        /// <summary>
+        /// Restituisce gli indici della tabella richiesta
+        /// </summary>
+        /// <param name="tableName">Nome tabella</param>
+        /// <returns>Indici della tabella</returns>
+        public string GetIndexList(string tableName);
+
+        /// <summary>
+        /// Crea Indice su un campo di una tabella di un db
+        /// </summary>
+        /// <param name="tableName">Nome tabella</param>
+        /// <param name="indexName">Nome Indice da creare</param>
+        public void CreateIndex(string tableName, string indexName);
+
+        /// <summary>
+        /// Elimina indice secondario costruito su un campo della tabella specificata
+        /// </summary>
+        /// <param name="tableName">Nome tabella</param>
+        /// <param name="indexName">Nome dell'indice</param>
+        public void DeleteIndex(string tableName, string indexName);
+
+        /// <summary>
+        /// Riconfigura il numero di shard e di repliche per ogni tabella sul db
+        /// </summary>
+        /// /// <param name="tableName">Nome tabella</param>
+        /// <param name="shards">Numero di Shards da impostare</param>
+        /// <param name="replicas"></param>
+        public void ReconfigureTable(string tableName, int shards, int replicas);
+
+        
+        
+
+    }
+}
