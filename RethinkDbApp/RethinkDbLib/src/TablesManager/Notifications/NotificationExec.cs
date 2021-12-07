33a5d2f02143dace6e8ed9f7595c86aff9e9c3e0
Francesco Foschini <francesco.foschini6@studio.unibo.it>
2021-12-07 17:28:16 +0100

    Implemented NotificationNewData and NotificationExecution

---
+++ RethinkDbApp/RethinkDbLib/src/TablesManager/Notifications/NotificationExec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RethinkDbLib.src.TablesManager.Notifications
+{
+    /// <summary>
+    /// Le Notifiche di Esecuzione servono per capire e intercettare la conclusione di un processo/task.
+    /// </summary>
+    public class NotificationExec : Notification
+    {
+        public NotificationExec() : base()
+        {
+
+        }
+        /// <summary>
+        /// Id di esecuzione
+        /// </summary>
+        public Guid IdExec { get; set; }
+
+        public override String ToString()
+        {
+            string result = base.ToString() + " IdExec: " + this.IdExec.ToString();
+            return result;
+        }
+    }
+}
---
+++ RethinkDbApp/RethinkDbLib/src/TablesManager/Notifications/NotificationNewData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RethinkDbLib.src.TablesManager.Notifications
+{
+    /// <summary>
+    /// Le notifiche di tipo NewData serviranno ad aggiornare la cache.
+    /// Quando verrà fatta una insert/update sul db "vero" queste notifiche permetterano di aggiungere/aggiornare il nuovo dato in cache.
+    /// </summary>
+    public class NotificationNewData: Notification
+    {
+        /// <summary>
+        /// Tabella del db "vero" in cui verrà fatta la insert/update
+        /// </summary>
+        public string Table { get; set; }
+
+        public override String ToString()
+        {
+            string result = base.ToString() + " Table: " + this.Table;
+            return result;
+        }
+    }
+}
