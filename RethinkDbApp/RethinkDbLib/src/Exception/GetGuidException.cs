96adba6bbb516f70a68889cd08292c73d10ffffa
Francesco Foschini <francesco.foschini6@studio.unibo.it>
2021-12-07 17:28:46 +0100

    Implemented two type of exception

---
+++ RethinkDbApp/RethinkDbLib/src/Exception/GetGuidException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RethinkDbLib.src.Exception
+{
+    /// <summary>
+    /// Se il Guid non è presente sul db Rethink
+    /// </summary>
+    [Serializable]
+    public class GetGuidException : System.Exception
+    {
+        private readonly static string message = "Il Guid non è presente sul db";
+        public GetGuidException() : base(message)
+        {
+
+        }
+    }
+}
---
+++ RethinkDbApp/RethinkDbLib/src/Exception/NewGuidException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RethinkDbLib.src.Exception
+{
+    /// <summary>
+    /// Se il Guid creato è già presente
+    /// </summary>
+    [Serializable]
+    public class NewGuidException : System.Exception
+    {
+        private readonly static string message = "Il Guid creato è già presente";
+        public NewGuidException() : base(message)
+        {
+
+        }
+    }
+}
