eca6413d6608d7364fd9c84c3c1b3e36e0454845
Francesco Foschini <francesco.foschini6@studio.unibo.it>
2021-12-07 17:22:26 +0100

    Implemented NotificationsManager

--- RethinkDbApp/RethinkDbLib/src/TablesManager/Notifications/NotificationsManager.cs
+++ RethinkDbApp/RethinkDbLib/src/TablesManager/Notifications/NotificationsManager.cs
@@ -6,20 +6,23 @@ namespace RethinkDbLib.src.TablesManager.Notifications
     class NotificationsManager : INotificationsManager
     {
         private readonly IConnectionNodes connection;
+        private readonly IQueryNotifications queryToNotifications;
 
         public NotificationsManager(IConnectionNodes connection)
         {
             this.connection = connection;
+            this.queryToNotifications = new QueryNotifications(connection);
         }
 
         public INotifier<T> GetNotifier<T>() where T : Notification
         {
-            throw new NotImplementedException();
+            INotifier<T> notifier = new Notifier<T>(this.connection);
+            return notifier;
         }
 
         public IQueryNotifications GetQueryService()
         {
-            throw new NotImplementedException();
+            return this.queryToNotifications;
         }
 
         public string GetWellKnownTable()
